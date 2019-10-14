9e23f5220dbef6887df130567a3fac8bce3aa643
Cian Payne <cianjpayne@gmail.com>
2019-10-14 16:55:28 +0100

    Got basic elo rating working

--- bestof.py
+++ bestof.py
@@ -1,22 +1,30 @@
 from random import choice
 from sys import argv
 
+from eduardo.eduardo import Elo
+
 def read_songs_in():
     with open(argv[1], 'r') as file:
         songlist = file.readlines()
     return songlist
 
-def get_songs_compare(songlist):
+def get_songs_to_compare(songlist):
     song1 = choice(songlist)
-    songlist.remove(song1)
-
     song2 = choice(songlist)
-    songlist.remove(song2)
-    # Must remove songs after selection to avoid duplicates
 
-    return (song1, song2, songlist)
+    while song1 == song2:
+        song2 = choice(songlist)
+        # Have to ensure that a song is not being
+        # put against itself
 
-def compare_songs(song1, song2):
+    return (song1, song2)
+
+def round(songlist):
+    song1_name, song2_name = get_songs_to_compare(songlist)
+
+    winner_name = get_user_choice(song1_name, song2_name)
+
+def get_user_choice(song1, song2):
     print("Which song is better?")
     print("A:", song1, sep=' ')
     print("B:", song2, sep=' ')
@@ -27,75 +35,44 @@ def compare_songs(song1, song2):
         ans = input(">>>")
 
     if ans == 'A':
-        return song1
+        return (song1, song2)
    
     else:
-        return song2
-
-def compare_three(songlist, num_to_return):
-    """
-    num_to_return param determine,s whether to
-    return the best song or the two best songs out
-    of a triple. Only accepted values are
-    1 (int) and 2 (int).
-    For the sake of consistency, the function
-    returns either a singleton or a double, depending
-    on num_to_return.
-    """
-
-    song_dict = {'A': songlist[0], 'B':songlist[1], 'C':songlist[2]}
-
-    print("Rank these songs:")
-    print("A:", songlist[0], sep=' ')
-    print("B:", songlist[1], sep=' ')
-    print("C:", songlist[2], sep=' ')
-
-    first = song_dict[input("1st: ").upper()]
-    second = song_dict[input("2nd: ").upper()]
-    third = song_dict[input("3rd").upper()]
-
-    if num_to_return == 1:
-        return (first)
-    else:
-        return (fist, second)
+        return (song2, song1)
 
 def write_list_to_file(songlist):
     filename = "top_{}.txt".format(len(songlist))
     with open(filename, 'w') as file:
         file.writelines(songlist)
 
-def round(songlist):
-    new_songlist = []
-
-    while len(songlist) > 3:
-        song1, song2, songlist = get_songs_compare(songlist)
-        # just checking that the size of songlist is decreaseing
-
-        new_songlist.append(compare_songs(song1, song2))
-
-    if len(songlist) == 2:
-        new_songlist.append(compare_songs(songlist[0], songlist[1]))
-        return new_songlist
-    
-    if len(new_songlist) % 2 == 0:
-        best_two = compare_three(songlist, 2)
-        for _ in best_two: new_songlist.append(_)
-    else:
-        best = compare_three(songlist, 1)
-        for _ in best: new_songlist.append(_)
-
-    return new_songlist
-
-def main():
+def main(num_of_rounds):
     songlist = read_songs_in()
+    elo_engine = Elo()
+    songs = [elo_engine.create_player(song) for song in songlist]
 
-    while len(songlist) > 2:
-        songlist = round(songlist)
-        write_list_to_file(songlist)
+    round_num = 0
 
-    best_song = compare_songs(songlist[0], songlist[1])
-    with open("best_song.txt", 'w') as file:
-        file.write(best_song)        
+    while round_num < int(num_of_rounds):
+        song1_name, song2_name = get_songs_to_compare(songlist)
+
+        winner_name, loser_name = get_user_choice(song1_name, song2_name)
+        winner = elo_engine.find_player(winner_name)
+        loser = elo_engine.find_player(loser_name)
+
+        winner.beat(loser)
+        round_num += 1
+
+    songlist_with_elo = {}
+    for song_name in songlist:
+        song = elo_engine.find_player(song_name)
+        songlist_with_elo[song_name] = song.rating
+
+    songs_sorted_by_elo = sorted(songlist_with_elo, key=songlist_with_elo.__getitem__)
+    # Getting a list of songs sorted by their elo
+
+    with open("ranked_songs_with_elo.txt", 'w') as file:
+        for song in songs_sorted_by_elo:
+            file.write(song + ' Elo: ' + str(elo_engine.find_player(song).rating) + '\n')
 
 if __name__ == "__main__":
-    main()
+    main(argv[2])
